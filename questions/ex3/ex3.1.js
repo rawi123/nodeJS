ef8431001cd2215a6e9e0bae7c1ba2bbffe4b047
Rawi Lahiany <rawipro.rl@gmail.com>
2021-11-07 15:01:45 +0200

    a

--- questions/ex3/ex3.1.js
+++ questions/ex3/ex3.1.js
@@ -1,7 +1,18 @@
 import { name,add,reduce,sayName } from "./exportFrom.mjs";
 import fs from 'fs';
 fs.writeFileSync("rawi.txt","this is imported fs");
+//cannot import env files
 console.log(name);
 console.log(add(1,2));
 console.log(reduce(5,2));
 console.log(sayName("rawi"));
+// import vs require:
+// require can be called anywhere import in the beggining
+
+// require only work with js
+
+// ES modules can be loaded dynamically via the import() function unlike require().
+// require not able to load partially while import can
+
+// import -export
+// require -module.exports
